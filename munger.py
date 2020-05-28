37cc2f1dd3be7eca4d03d1cce08ba8c0070a98b4
John Smith <mitch@jmitchellsmith.com>
2020-05-28 14:38:46 -0500

    Added google trends scraper.

--- munger.py
+++ munger.py
@@ -128,21 +128,57 @@ class HeavyScraper:
         return '<HeavyScraper object: url={}>'.format(self.url)
 
 
+class Trends(HeavyScraper):
+    """
+    Top Google Search terms scraped from Google Trends
+    """
+    url = "https://trends.google.com/trends/trendingsearches/daily?geo=US"
+
+    def __init__(self):
+        """ Fetches a list of trending google search terms and immediately close the marionette driver  """
+        super().__init__(self.url)
+        self.driver.get(self.url)
+        self._trends = [
+                (topic.text.split("\n")[1], topic.text.split("\n")[2], topic.text.split("\n")[6])
+                for topic
+                in self.driver.find_elements_by_class_name('feed-item')
+            ]
+        self.driver.close()
+    
+    @property
+    def trends(self):
+        return self._trends
+
+    @property
+    def ngrams(self):
+        return [n[0] for n in self._trends]
+ 
+    def __repr__(self):
+        return '<Trends object: url={}>'.format(self.url)
+
 
 
 if __name__ == "__main__":
+    import unittest
 
-    def test_instantiate_heavy_scraper():
-        """ Test instantiate HeavyScraper object. """
-        s = HeavyScraper()
-        try:
-            assert repr(s) == '<HeavyScraper object: url=None>'
-            print("Passed: test_instantiate_heavy_scraper")
-        except Exception as Err:
-            print("Failed: ", Err)
-        s.driver.close()
+    class TestSeleniumScrapers(unittest.TestCase):
 
+        def test_instantiate_heavy_scraper(self):
+            """ Test instantiate HeavyScraper object. """
+            o = HeavyScraper()
+            self.assertEqual(repr(o), "<HeavyScraper object: url=None>", "incorrect object representation")
+            o.driver.close()
 
-    print('Running unit tests . . .')
-    test_instantiate_heavy_scraper()
-    print('Everything Passed.')
+        def test_instantiate_trends_object(self):
+            print("Fetching data; please be patient . . .")
+            o = Trends()
+            self.assertEqual(
+                    repr(o),
+                    "<Trends object: url=https://trends.google.com/trends/trendingsearches/daily?geo=US>",
+                    "incorrect object representation"
+                    )
+            self.assertTrue(len(o.ngrams) > 0, "no data was fetched")
+        
+
+    unittest.main()
+
