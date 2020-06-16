b3d4243daa2232a4cf4903f58af9a292f840e75b
John Smith <mitch@jmitchellsmith.com>
2020-06-16 11:23:26 -0500

    Switching from nltk to spaCy, but keeping nltk as a dependency; refactoring a bit.

--- munger.py
+++ munger.py
@@ -7,42 +7,62 @@ scraped web pages.
 
 import os
 import re
+import datetime
 import time
 import json
+import pickle
 import spacy
+from spacy.lang.en import English
+from spacy.tokens import Doc, Span, Token
+from spacy.matcher import PhraseMatcher
 from collections import deque
 from scrapers import Trends, Aggregator, APHeadlines, APArticle 
 
+
+
+def load_or_refresh_ag():
+    cached = datetime.date.today().strftime("tmp/ag_%Y%m%d.pkl")
+    if os.path.isfile(cached):
+        with open(cached, "rb") as pkl:
+            ag = pickle.load(pkl)
+    else:
+        ag = Aggregator()
+        ag.collect_ap_headlines()
+
+        for url in [h[1] for h in ag.headlines if h[0] == 'Sports']:
+            try:
+                ag.fetch_ap_article(url)
+            except:
+                continue
+            if len(ag.stories) >= 2:
+                break
+
+        for story in ag.stories:
+            # ditch unpicklable
+            story.driver = None
+
+        with open(cached, "wb") as pkl:
+            pickle.dump(ag, pkl)
+        
+    return ag
+
+
+ag = load_or_refresh_ag()
+
 nlp = spacy.load("en_core_web_sm")
 
-ag = Aggregator()
-ag.collect_ap_headlines()
+Doc.set_extension("title", default=None)
+Doc.set_extension("byline", default=None)
+Doc.set_extension("timestamp", default=None)
+Doc.set_extension("dateline", default=None)
 
-for url in [h[1] for h in ag.headlines if h[0] == 'Sports']:
-    try:
-        ag.fetch_ap_article(url)
-    except:
-        continue
-    if len(ag.stories) >= 2:
-        break
+docs = []
+for i, story in enumerate(ag.stories):
+    docs.append((nlp("\n".join(story.content))))
+    docs[i]._.title = story.title
+    docs[i]._.byline = story.byline
+    docs[i]._.timestamp = story.timestamp
 
-# Import the Matcher
-from spacy.matcher import Matcher
 
-nlp = spacy.load("en_core_web_sm")
-doc = nlp("Upcoming iPhone X release date leaked as Apple reveals pre-orders")
-
-# Initialize the Matcher with the shared vocabulary
-matcher = Matcher(nlp.vocab)
-
-# Create a pattern matching two tokens: "iPhone" and "X"
-pattern = [{"TEXT": "iPhone"}, {"TEXT": "X"}]
-
-# Add the pattern to the matcher
-matcher.add("IPHONE_X_PATTERN", None, pattern)
-
-# Use the matcher on the doc
-matches = matcher(doc)
-print("Matches:", [doc[start:end].text for match_id, start, end in matches])
 
 
