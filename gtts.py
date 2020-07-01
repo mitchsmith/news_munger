37c4944b5a23b95b563d6fac50156af743d54aa6
John Smith <mitch@jmitchellsmith.com>
2020-07-01 15:39:38 -0500

    Added google-text-to speech client.

---
+++ gtts.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""  """
+
+from google.cloud import texttospeech as tts
+
+def list_voices(language_code=None):
+    client = texttospeech.TextToSpeechClient()
+    response = client.list_voices(language_code)
+    voices = sorted(response.voices, key=lambda voice: voice.name)
+
+    print(f' Voices: {len(voices)} '.center(60, '-'))
+    for voice in voices:
+        languages = ', '.join(voice.language_codes)
+        name = voice.name
+        gender = enums.SsmlVoiceGender(voice.ssml_gender).name
+        rate = voice.natural_sample_rate_hertz
+        print(f'{languages:<8}',
+              f'{name:<24}',
+              f'{gender:<8}',
+              f'{rate:,} Hz',
+              sep=' | ')
+
+
+def text_to_wav(voice_name, text):
+    language_code = '-'.join(voice_name.split('-')[:2])
+    text_input = tts.SynthesisInput(text=text)
+    voice_params = tts.VoiceSelectionParams(
+        language_code=language_code,
+        name=voice_name)
+    audio_config = tts.AudioConfig(
+        audio_encoding=tts.AudioEncoding.LINEAR16)
+
+    client = tts.TextToSpeechClient()
+    response = client.synthesize_speech(
+        input=text_input,
+        voice=voice_params,
+        audio_config=audio_config)
+
+    filename = f'{language_code}.wav'
+    with open(filename, 'wb') as out:
+        out.write(response.audio_content)
+        print(f'Audio content written to "{filename}"')
+
+
