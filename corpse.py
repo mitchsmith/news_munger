df1007bcfab92cb4194e09e63e3d6906d7bf3703
John Mitchell Smith <jmsdomo@gmail.com>
2020-08-14 09:46:48 -0500

    Merged changed to corpse.py from playground.

--- corpse.py
+++ corpse.py
@@ -22,96 +22,39 @@ class ExquisiteCorpse(Munger):
     
     """  """
 
+    def __init__(self, documents):
+        super().__init__(documents)
+        self.corpses = []
+
     def build(self):
-        pass
-    
-    def munge_sayings(self, sentences):
-        print("Munging Sayings:")
-        for sentence in sentences:
-            if sentence[3][0].tag_ == "``":
-                # begins with a quotation
-                pattern = re.compile(r"^(“)([^”]+)([,\.]”)(.*)$")
-                tmp = nlp(pattern.sub(r"\2.", sentence[3].text))
-                s = next(islice(tmp.sents, 0, None))
-                repl = self.munge_on_roots((None, None, s.root.lemma_, s))
-                if type(repl) == Doc:
-                    new = pattern.sub(r"\1{}\3\4".format(repl), sentence[3].text_with_ws)
-                    return new
+        text = ""
+        base_index = random.randrange(len(self._documents))
+        base = self._documents[base_index]
+        sentences = []
+        for i, s in enumerate(base.sents):
+            stuple = (base_index, i, s.root.lemma_, s)
+            if stuple[2] == 'say':
+                sentence = self.munge_sayings(stuple)
+            elif stuple[2] in ['be', 'do', 'have']:
+                sentence = self.munge_children(stuple)
             else:
-                lefts = []
-                rights = []
-                for right in sentence[3].root.rights:
-                    rights.extend([t for t in right.children])
-                if rights and rights[0].tag_ == "``":
-                    try:
-                        end = [t.tag_ for t in rights[1:]].index("``")
-                        tmp = nlp("".join([t.text_with_ws for t in rights[1:end]]))
-                        s = next(islice(tmp.sents, 0, None))
-                        pattern = re.compile(r"^(“)([^”]+)([,\.]”)(.*)$")
-                        repl = self.munge_on_roots((None, None, s.root.lemma_, s))
-                        if type(repl) == Doc:
-                            new = pattern.sub(r"\1{}\3\4".format(repl),
-                                    sentence[3].text_with_ws
-                                    )
-                        return new
-
-                    except ValueError:
-                        pass
+                sentence = self.munge_on_roots(stuple)
                 
-                sentence = []
-                while type(sentence) == list:
-                    sentence = self.munge_on_roots(
-                            sentences[random.randrange(len(sentences))]
-                            )
-                    sentences = sentence
-                        
-                return sentence
-        
-        return sentences
-    
-    
-    def munge_beings(self, sentences):
-        print("Munging Beings:")
-        sentence = sentences[random.randrange(len(sentences))][3]
-        text = sentence.text_with_ws
-        for left in sentence.root.lefts:
-            if left.dep_ != 'punct':
-                orig = "".join([t.text_with_ws for t in left.subtree])
-                alternates = self._be_children['left'][left.dep_]
-        alt = alternates[random.randrange(len(alternates))]
-        repl = "".join([t.text_with_ws for t in alt[2]])
-        if not repl:
-            repl = " "
-        repl = " {}".format(repl)
-        text = re.sub(orig, repl, text)
-        text = re.sub(r"\s+", " ", text)
-        text = re.sub(r"\s([,;?!.])", r"\1", text)
-        for right in sentence.root.rights:
-            if right.dep_ != 'punct':
-                orig = "".join([t.text_with_ws for t in right.subtree])
-                alternates = self._be_children['right'][right.dep_]
-        alt = alternates[random.randrange(len(alternates))]
-        repl = "".join([t.text_with_ws for t in alt[2]])
-        if not repl:
-            repl = " "
-        repl = " {}".format(repl)
-        text = re.sub(orig, repl, text)
-        text = re.sub(r"\s+", " ", text)
-        text = re.sub(r"\s([,;?!.])", r"\1", text)
-        text =  re.sub(r"(\w)( but )", r"\1 and ", text)
-        text = re.sub(r"^\s+", "", text)
-        sentence = nlp(text)
+            sentences.append(sentence)
 
-        return sentence
-    
-    
-    def munge_children(self, sentences):
-        if sentences[1][2] == "be":
-            return self.munge_beings(sentences)
-        
-        return self.munge_sayings(sentences)
-        
-        
+        self.corpses.append({'title': base._.title, 'sentences': sentences}) 
+
+        text += "\n".join([sent[-1].text_with_ws for sent in sentences])
+        print(text)
+
+    def save(self):
+        self.filename = datetime.datetime.today().strftime("tmp/exq_%Y%m%d.txt")
+        pass
+
+
+
+
+       
     def __repr__(self):
         return "<ExquisiteCorpse: {}>".format(self.headline)
 
